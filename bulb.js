76c078ec10ac021af30559a8bf42f08057731e1b
khalidsamad123 <143835090+khalidsamad123@users.noreply.github.com>
2024-07-13 12:27:29 -0700

    first commit

---
+++ bulb.js
@@ -0,0 +1,11 @@
+ function bulbOn(){
+    let bulbobj = document.querySelector(".off-image");
+    bulbobj.setAttribute("src","toppng.com-light-bulb-on-off-png-1559x1701.png")
+
+ }
+
+ function bulbOff(){
+    let bulbobj = document.querySelector(".off-image");
+    bulbobj.setAttribute("src","toppng.com-light-bulb-on-off-png-512x512.png")
+
+ }
